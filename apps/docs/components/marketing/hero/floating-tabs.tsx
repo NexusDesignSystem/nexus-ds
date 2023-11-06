fd8be1fdf8a2bac5d1f9fc141a9680336dbb9055
The-Beyonder <kanishk.ks@somaiya.edu>
2023-11-07 00:45:56 +0530

    feat: docs

--- apps/docs/components/marketing/hero/floating-tabs.tsx
+++ apps/docs/components/marketing/hero/floating-tabs.tsx
@@ -9,14 +9,14 @@ export const FloatingTabs: React.FC<{}> = () => {
       className=""
       classNames={{
         base: "absolute left-[170px] -top-[160px] h-10 animate-[levitate_17s_ease_infinite_1s]",
-        tabList: "max-w-[200px] shadow-sm",
+        tabList: "max-w-[250px] shadow-sm",
       }}
       radius="full"
       size="sm"
     >
       <Tab key="beyonder" title="Beyonder" />
       <Tab key="delta" title="Delta" />
-      <Tab key="bee" title="Bee" />
+      <Tab key="bee" title="BusyBee" />
     </Tabs>
   );
 };
