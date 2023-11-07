8d9c43bb5c9fb877e7e5a1489fb528a32e3f9bef
The-Beyonder <kanishk.ks@somaiya.edu>
2023-11-07 20:54:16 +0530

    feat(docs): new homescreen-tagline

--- apps/docs/components/marketing/hero/hero.tsx
+++ apps/docs/components/marketing/hero/hero.tsx
@@ -23,8 +23,8 @@ export const Hero = () => {
           <div className="inline-block">
             <h1 className={title()}>Develop&nbsp;</h1>
             <h1 className={title({color: "blue"})}>beautiful&nbsp;</h1>
-          <h1 className={title()}>websites effortlessly.</h1>
           </div>
+          <h1 className={title()}>websites effortlessly.</h1>
         </div>
         <h2 className={subtitle({fullWidth: true, class: "text-center md:text-left"})}>
           The ultimate modern design system.
